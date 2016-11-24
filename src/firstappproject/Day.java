b092aec438cb36788ebc4c3f96d45fac3a4383ce
1404610 <1404610@Y348SDP21.ad.dundeeandangus.ac.uk>
2016-11-24 10:02:21 +0000

    1.0 project

---
+++ src/firstappproject/Day.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstappproject;
+
+/**
+ *
+ * @author 1404610
+ */
+public class enum Day {
+       Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+
+}
